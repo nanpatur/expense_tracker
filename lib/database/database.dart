6e556dba5f7c7204220cc90c6ed2f6a6a2ab1f45
Nandira Paturohman <nandirasp@gmail.com>
2024-09-27 15:22:49 +0700

    Refactor database.dart: Remove unnecessary comments and improve code structure

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,19 +1,13 @@
-// the code below is used to create a class database for creating the sqlite database
-// for our project
 import 'dart:io';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseProvider {
-  // the code below is used to create a static final instance of the Database provider class
   static final DatabaseProvider dbProvider = DatabaseProvider();
 
-  // the code below is used to create an instance of the database from our sqflite package
   Database? database;
 
-  // the code below is used to create a getter for checking that if the database
-  // is not null then returning the database else creating a new database
   Future<Database> get db async {
     if (database != null) {
       return database!;
@@ -23,20 +17,13 @@ class DatabaseProvider {
     }
   }
 
-  // the code below is used to create a method to create the database
   Future<Database> createDatabase() async {
-    // the code below is used to get the location of the application document directory
     Directory docDirectory = await getApplicationDocumentsDirectory();
-    // the code below is useed to get the path where our sqlite database will be located
-    // by using the join method
     String path =
-        join(docDirectory.path, "myExpenseDatabase.db"); // here myExpenseDatabase.db is the name of
-    // our database
+        join(docDirectory.path, "myExpenseDatabase.db");
 
-    // in the line of code below we need to use the openDatabase() method to
-    // open the database and create the table using raw sql command
     var database = await openDatabase(
-      path, // the path where our database is located
+      path,
       version: 1,
       onCreate: (Database db, int version) async {
         await db.execute("CREATE TABLE expenseTable ("
@@ -47,7 +34,6 @@ class DatabaseProvider {
             "category INTEGER "
             ")");
       },
-
          
       onUpgrade: (Database db, int oldVersion, int newVersion) {
         if (newVersion > oldVersion) {}
